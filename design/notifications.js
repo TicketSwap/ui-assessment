1f86549c890fe1b381eae017790a296419c4481b
Rob Vermeer <rob@vermeertech.nl>
2017-11-20 00:23:07 +0100

    Initial commit

---
+++ design/notifications.js
@@ -0,0 +1,62 @@
+var notifications = [
+    {
+        name: 'Linde',
+        avatar: 'https://cdn.ticketswap.com/public/201606/cb14645d-2b71-439d-92b6-feefd0e16c26.png',
+        event: 'Awakenings Festival 2017',
+        tickets: 4
+    },
+    {
+        name: 'Koen',
+        avatar: 'https://cdn.ticketswap.com/public/201604/9887d946-47a1-4e01-881b-6a33076c4de8.jpeg',
+        event: 'Welcome to the Future - Indoor Day Festival 2017',
+        tickets: 2
+    },
+    {
+        name: 'Julie',
+        avatar: 'https://cdn.ticketswap.com/public/201703/569309fc-2e3c-41c9-8e1a-36dcc63858b2.png',
+        event: 'Toffler Indoor Festival 2017',
+        tickets: 1
+    },
+    {
+        name: 'Pascal',
+        avatar: 'https://cdn.ticketswap.com/public/201601/e36b0348-cdc3-408d-a020-968a90ddbba9.jpeg',
+        event: 'Kensington',
+        tickets: 2
+    },
+    {
+        name: 'Nikki',
+        avatar: 'https://cdn.ticketswap.com/public/201707/76a10f7c-4707-4f66-a70f-8428d240a382.png',
+        event: 'Kendrick Lamar - The Damn. Tour',
+        tickets: 3
+    },
+    {
+        name: 'Mark',
+        avatar: 'https://cdn.ticketswap.com/public/201709/e6d4f11b-ac8b-4ee3-8a6d-04d1e34bc65f.png',
+        event: 'Shakira - El Dorado World Tour',
+        tickets: 10
+    },
+    {
+        name: 'Lauren',
+        avatar: 'https://cdn.ticketswap.com/public/201608/92326b92-ce4d-4cf3-8d69-032687a44a51.png',
+        event: 'Freaqshow 2017',
+        tickets: 2
+    },
+    {
+        name: 'Bob',
+        avatar: 'https://cdn.ticketswap.com/public/201601/d7ca4794-ce30-48fa-8753-3327c9ce713c.jpeg',
+        event: 'Imagine Dragons - Evolve World Tour',
+        tickets: 5
+    },
+    {
+        name: 'Tygra',
+        avatar: 'https://cdn.ticketswap.com/public/201707/b676dc31-66ba-4a55-aa0d-ba4f55dba7ac.png',
+        event: 'The Script',
+        tickets: 1
+    },
+    {
+        name: 'Lucas',
+        avatar: 'https://cdn.ticketswap.com/public/201612/d8b4e303-ce95-477e-982f-9e5801c14a5c.png',
+        event: 'Depeche Mode - Global Spirit Tour',
+        tickets: 3
+    },
+]
---
+++ readme.md
@@ -0,0 +1,14 @@
+# TicketSwap assessment
+Thank you for doing our ui frontend assessment. The goal of the assessment is a page with notifications that can be added with a button. If you have any questions you can always [contact us](mailto:rob@ticketswap.com).
+
+## Design
+The design is included in the repo at `./design`. Included is a Sketch file and some preview images.
+
+## Notifications data
+We provided an array with data for the notifications at `./design/notifications.js` for your convenience. You can copy paste this in your own project.
+
+## Handing in the assessment
+When you're finished you can send it to us in reply of the mail you got with this assessment.
+
+### Good luck
+And again if you have any questions, [let us know](mailto:rob@ticketswap.com)!
